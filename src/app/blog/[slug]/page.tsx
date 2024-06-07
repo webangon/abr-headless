91579b7e703866e9bea592637705afc98e561606
Md. Abdul Kader Roni <kaderuap@gmail.com>
2024-06-07 21:02:28 +0600

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,51 +1,51 @@
-import { Metadata } from 'next'
-import Header from '@/app/components/Header';
-import Footer from '@/app/components/Footer';
+// import { Metadata } from 'next'
+// import Header from '@/app/components/Header';
+// import Footer from '@/app/components/Footer';
  
-export const metadata: Metadata = {
-  title: 'Blog - Amazing Business Results',
-}
+// export const metadata: Metadata = {
+//   title: 'Blog - Amazing Business Results',
+// }
 
-export default async function Page({ params }: {
-    params: { slug: string };
-}) {
+// export default async function Page({ params }: {
+//     params: { slug: string };
+// }) {
 
-    const query = `
-    query {
-        postBy(slug: "${params.slug}") {
-            title
-            content
-        }   
-    }    
-    `;
+//     const query = `
+//     query {
+//         postBy(slug: "${params.slug}") {
+//             title
+//             content
+//         }   
+//     }    
+//     `;
 
-    const res = await fetch('https://webangon.com/abr/graphql', {
-        method: "POST",
-        cache: 'no-store',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-        body: JSON.stringify({
-            query
-        })
-    }).then(
-        response => {
-            return response.json();
-        }
-    ).then(
-        data => {
-            return data;
-        }
-    )
-    const pageData = res.data.postBy.content;
+//     const res = await fetch('https://webangon.com/abr/graphql', {
+//         method: "POST",
+//         cache: 'no-store',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//         },
+//         body: JSON.stringify({
+//             query
+//         })
+//     }).then(
+//         response => {
+//             return response.json();
+//         }
+//     ).then(
+//         data => {
+//             return data;
+//         }
+//     )
+//     const pageData = res.data.postBy.content;
 
-    return(
-        <>
-        <Header/>
-        <div dangerouslySetInnerHTML={{__html: pageData}}></div>
-        <Footer/>
+//     return(
+//         <>
+//         <Header/>
+//         <div dangerouslySetInnerHTML={{__html: pageData}}></div>
+//         <Footer/>
 
-        </>
-    ) 
-}
+//         </>
+//     ) 
+// }
