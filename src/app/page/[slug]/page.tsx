3c7c8807c46dc5a20fafb207ba0ed95206701165
webangon <webangon@users.noreply.github.com>
2024-06-20 13:17:52 +0600

    New CSS

--- src/app/page/[slug]/page.tsx
+++ src/app/page/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from 'next'
 import Header from '../.././components/Header'
 import Footer from "../.././components/Footer"
- 
+import axios from 'axios';
+
 export const metadata: Metadata = {
   title: 'Amazing Business Results',
 }
@@ -40,16 +41,17 @@ export default async function Page({ params }: {
             return data;
         }
     )
+
+    const metadata = await axios.get('https://webangon.com/abr/wp-json/rankmath/v1/getHead?url=https://webangon.com/abr/page/about/')
+    console.log(metadata.data.head);
     const pagedata = res.data.pages.nodes;
     return(
         <>
         <Header/>
-
       {pagedata.map((item:any) => {
         const content = item.content;
         return(
             <>
-          <h2>{item.title}</h2>
           <div dangerouslySetInnerHTML={{__html: content}}></div>            
             </>
         );
